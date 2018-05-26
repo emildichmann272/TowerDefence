2be361a9c48a67f27b86a09526bc3837a29b8be4
Andreas <andreas@localhost.localdomain>
2018-05-26 14:54:20 +0200

    Added basic css-grid overlay

--- views/js/main.js
+++ views/js/main.js
@@ -1,7 +1,27 @@
-var socket = io('http://93.161.6.30:3000');
+var socket = io('http://localhost:3000');
 
 socket.on('Test', function(data){
 	socket.emit('Test', data);
 	document.getElementById("serverData")
 	.innerHTML = data.replace('%20', ' ');
 });
+
+socket.on('createCollectionSuccess', function(result) {
+	document.getElementById("collectionStatus").innerHTML = "Creation of the collection was a success! :)";
+});
+
+socket.on('createCollectionFailed', function(result) {
+	document.getElementById("collectionStatus").innerHTML = "Creation of the collection failed :(";
+});
+
+var btn = document; //.getElementById('createCollectionBtn');
+btn.addEventListener('click', createCollection);
+
+function createCollection() {
+	console.log("createCollection");
+	if (document.getElementById("createCollectionName").value != '') {
+		socket.emit('createCollection', [document.getElementById("createCollectionName").innerHTML, null]);
+	} else {
+		console.log('Please insert a name!');
+	};
+};
